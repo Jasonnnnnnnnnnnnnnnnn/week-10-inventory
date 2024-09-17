41d8abe551d05d6626bd7ef897f4cd159e8923b0
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-09-17 19:45:19 +0700

    finish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
+import { ClientProxy, Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, @Inject('ORDER_SERVICE') private orderService: ClientProxy,) { }
 
   @Get()
   getHello(): string {
@@ -12,9 +12,24 @@ export class AppController {
   }
 
   @MessagePattern('order_created')
-  handleOrderCreated(@Payload() data:any, @Ctx() context: RmqContext) {
-    console.log(`Pattern: ${context.getPattern()}`);
-    console.log(context.getMessage());
-    console.log(context.getChannelRef());
+  handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    // console.log(`Pattern: ${context.getPattern()}`);
+    // console.log(context.getMessage());
+    // console.log(context.getChannelRef());
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    console.log('Order received for processing:', data);
+    const isInStock = true;
+    if (isInStock) {
+      console.log('Inventory available. Processing order.');
+      channel.ack(originalMsg);
+      // Completed Order
+      this.orderService.emit('order_completed', data);
+    } else {
+      console.log('Inventory not  available.');
+      channel.ack(originalMsg);
+      // Canceld Order
+      this.orderService.emit('order_canceld', data);
+    }
   }
 }
