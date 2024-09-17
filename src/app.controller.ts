e92f2aa617579dc8f75bf52f36cd73abaec85392
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-09-17 19:15:09 +0700

    Handle messages from RabbitMQ using MessagePattern

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('order_created')
+  handleOrderCreated(@Payload() data:any, @Ctx() context: RmqContext) {
+    console.log(`Pattern: ${context.getPattern()}`);
+    console.log(context.getMessage());
+    console.log(context.getChannelRef());
   }
 }
